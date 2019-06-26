fea612b3a67839024f3d6fe57635fade5bc27e9e
amenpunk <ondasycircuitos@gmail.com>
2019-06-25 18:45:20 -0600

    config to deploy

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 
-define ("base_url", "http://localhost/Tienda/");
+define ("base_url", "https://berserkstore.azurewebsites.net/");
 define ("controller_default", "productosController");
 define ("action_default","index");
